1655e9ac02c9907976d1d1cd27c37284250b836a
izzaafkarinank <izzaafkarina14@gmail.com>
2024-12-24 18:41:38 +0700

    add global state to for add notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import ActiveNotes from "./components/active-notes";
 import CreateNotes from "./components/create-notes";
 import Navbar from "./components/navbar";
+import { getInitialData } from "./utils/data";
 
 function App() {
+  const [notes, setNotes] = useState(getInitialData());
+
+  const addNote = (title: string, body: string) => {
+    const newNote = {
+      id: notes.length + 1,
+      title,
+      body,
+      createdAt: new Date().toISOString(),
+      archived: false
+    };
+    setNotes([...notes, newNote]);
+  };
+  
   return (
     <>
       <Navbar />
@@ -15,7 +30,7 @@ function App() {
           margin: "16px auto",
         }}
       >
-        <CreateNotes />
+        <CreateNotes/>
       </div>
       <ActiveNotes />
     </>
