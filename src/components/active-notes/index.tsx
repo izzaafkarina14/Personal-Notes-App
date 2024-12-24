af96a29718a7d536be37c22dacf8b4f41808da5d
izzaafkarinank <izzaafkarina14@gmail.com>
2024-12-24 19:19:28 +0700

    add 'tidak ada catatan' message when notes is empty

--- src/components/active-notes/index.tsx
+++ src/components/active-notes/index.tsx
@@ -2,42 +2,51 @@ import { showFormattedDate } from "../../utils/data";
 import styles from "./style.module.css";
 
 type Note = {
-  id: number,
-  title: string,
-  body: string,
-  createdAt: string,
-  archived: boolean
+  id: number;
+  title: string;
+  body: string;
+  createdAt: string;
+  archived: boolean;
 };
 
 type ActiveNotesProps = {
-  notes: Note[]
-  deleteNote: (id: number) => void
-}
+  notes: Note[];
+  deleteNote: (id: number) => void;
+};
 
-export default function ActiveNotes({notes, deleteNote}: ActiveNotesProps) {
+export default function ActiveNotes({ notes, deleteNote }: ActiveNotesProps) {
   return (
     <>
       <div className={styles.activeNotes}>
         <h2>Catatan Aktif</h2>
-        <div className={styles.notesList}>
-          {notes.map((note) => (
-            <div className={styles.notesCard} key={note.id}>
-              <div className={styles.noteCardContect}>
-                <h3>{note.title}</h3>
-                <p className={styles.date}>
-                  {showFormattedDate(note.createdAt)}
-                </p>
-                <p className={styles.noteBody}>{note.body}</p>
+        {notes.length === 0 ? (
+          <p className={styles.emptyMessage}>Tidak ada catatan</p>
+        ) : (
+          <div className={styles.notesList}>
+            {notes.map((note) => (
+              <div className={styles.notesCard} key={note.id}>
+                <div className={styles.noteCardContect}>
+                  <h3>{note.title}</h3>
+                  <p className={styles.date}>
+                    {showFormattedDate(note.createdAt)}
+                  </p>
+                  <p className={styles.noteBody}>{note.body}</p>
+                </div>
+                <div className={styles.noteCardAction}>
+                  <button
+                    className={styles.delete}
+                    onClick={() => {
+                      deleteNote(note.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                  <button className={styles.archive}>Arsipkan</button>
+                </div>
               </div>
-              <div className={styles.noteCardAction}>
-                <button className={styles.delete} onClick={() => {
-                  deleteNote(note.id)
-                }}>Delete</button>
-                <button className={styles.archive}>Arsipkan</button>
-              </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
